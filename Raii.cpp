0beb7128de21f5eebb026c868550efe99d56d6d1
Sam Brezovsiy <arsikqa43@gmail.cpp>
2024-10-01 17:47:02 +0700

    Construtor has been created. Constructor this is from making objects (Class Objectclass;). Constr. making automatyc by compile, but if you make this by hands, you can specific this.

--- Raii.cpp
+++ Raii.cpp
@@ -1,10 +1,23 @@
-#include<iostream>
-
+#include <iostream>
+ 
+class Person 
+{
+public:
+    std::string name;
+    unsigned age;
+    void print() 
+    {
+        std::cout << "Name: " << name << "\tAge: " << age << std::endl;
+    }
+    Person(std::string p_name, unsigned p_age)
+    {
+        name = p_name;
+        age = p_age;
+        std::cout << "Person has been created" << std::endl;
+    }
+};
 int main()
 {
-     
+    Person tom("Tom", 38);  // создаем объект - вызываем конструктор
+    tom.print();
 }
-class SomeClass
-{
-
-};
